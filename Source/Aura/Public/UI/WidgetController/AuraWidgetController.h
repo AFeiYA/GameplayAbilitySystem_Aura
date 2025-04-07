a73e1bcf5f34f890a20674551e32bede4665e7a1
AFeiYA <386665678@qq.com>
2025-04-07 22:34:00 +0800

    5.6 SetParams from USTRUCT

--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -3,11 +3,38 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
+#include "AbilitySystem/AuraAttributeSet.h"
 #include "AuraWidgetController.generated.h"
 
 class UAttributeSet;
 class UAbilitySystemComponent;
+
+// 该结构体的目的是 将UI控件所需的核心组件聚合在一起，方便在以下场景中传递参数：
+// 初始化UI控件：在创建HUD、角色属性面板时，需要引用玩家控制器、状态、技能系统等。
+// 数据绑定：将UI元素与 AbilitySystemComponent 或 AttributeSet 动态绑定，实现属性实时更新。
+// 蓝图配置：允许设计师在编辑器中灵活配置参数，无需修改C++代码。
+USTRUCT(BlueprintType)// 声明为可在蓝图中使用的结构体
+struct FWidgetControllerParams
+{
+	GENERATED_BODY() // 必须的宏，生成反射代码
+
+	FWidgetControllerParams() {}
+	FWidgetControllerParams(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS)
+		: PlayerController(PC), PlayerState(PS),AbilitySystemComponent(ASC), AttributeSet(AS) {}
+		
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	TObjectPtr<APlayerController> PlayerController = nullptr;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	TObjectPtr<APlayerState> PlayerState=nullptr;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent=nullptr;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	TObjectPtr<UAttributeSet> AttributeSet=nullptr;
+};
+
 /**
  * 
  */
@@ -15,6 +42,10 @@ UCLASS()
 class AURA_API UAuraWidgetController : public UObject
 {
 	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetWidgetControllerParams(const FWidgetControllerParams& WCParams);
+	
 protected:
 	
 	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
