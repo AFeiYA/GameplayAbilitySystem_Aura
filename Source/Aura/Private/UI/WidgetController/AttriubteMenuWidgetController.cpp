bbe8b1d4960a2eb42910703b8961b17c24ed4861
AFeiYA <386665678@qq.com>
2025-05-20 09:36:31 +0800

    9.15 Attribute Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttriubteMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/WidgetController/AttriubteMenuWidgetController.h"
+
+void UAttriubteMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
+void UAttriubteMenuWidgetController::BroadcastInitialValues()
+{
+}
