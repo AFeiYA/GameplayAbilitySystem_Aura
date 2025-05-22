4cc7fb7e498d7473baec3f2f1ccc47504a150002
AFeiYA <386665678@qq.com>
2025-05-22 20:21:01 +0800

    9.18 Attribute Info Delegate

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,10 +3,19 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+#include "Animation/AnimRootMotionProvider.h"
+
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
 }
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
+	UAuraAttributeSet* AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
+	check(AttributeInfo);
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
+	Info.AttributeValue = AuraAttributeSet->GetStrength();
+	AttributeInfoDelegate.Broadcast(Info);
 }
--- Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -6,15 +6,26 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "AttributeMenuWidgetController.generated.h"
 
+
+class UAttributeInfo;
+struct FAuraAttributeInfo;
+// 该结构体的目的是 将UI控件所需的核心组件聚合在一起，方便在以下场景中传递参数：
+// Data binding: 将UI元素与 AbilitySystemComponent 或 AttributeSet 动态绑定，实现属性实时更新。
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignature, const FAuraAttributeInfo&, Info);
 /**
  * 
  */
-
 UCLASS(BlueprintType, Blueprintable)
 class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
 {
 	GENERATED_BODY()
 public:
 	virtual void BindCallbacksToDependencies() override;
-	virtual void BroadcastInitialValues() override;	
+	virtual void BroadcastInitialValues() override;
+
+	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
+	FAttributeInfoSignature AttributeInfoDelegate;
+protected:
+	UPROPERTY(EditDefaultsOnly, Category="GAS|Attributes")
+	TObjectPtr<UAttributeInfo> AttributeInfo;
 };
